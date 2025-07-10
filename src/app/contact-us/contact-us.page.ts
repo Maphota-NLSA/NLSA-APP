1425e5053e040de3a435d13754816832d3095e1c
Maphota-NLSA <Maphota.Shiburi@nlsa.ac.za>
2025-07-10 13:47:02 +0200

    Cards tittle inherit the colors including page header title, icons are visibible, home image back to swiper, labels listed as mentioned. Video functionality functioning.

--- src/app/contact-us/contact-us.page.ts
+++ src/app/contact-us/contact-us.page.ts
@@ -11,27 +11,27 @@ export class ContactUsPage implements OnInit {
 
       cards = [
     {
-      title: 'NLSA Collections',
-      description: 'Search through the NLSA Catalogue.',
+      title: 'Search the Catalogue ',
+      description: 'Search through the NLSA Digital Collections.',
       link: 'https://nlsa.on.worldcat.org/discovery',
       icon: 'book',
     },
     {
       title: 'Digital Records',
-      description: 'Search through our Digital records.',
+      description: 'Search our Digital Collections.',
       link: 'https://cdm21048.contentdm.oclc.org/digital',
       icon: 'briefcase',
     },
     {
       title: 'ISBN',
-      description: 'Appl for your ISBN number',
+      description: 'Apply for an ISBN number.',
       link: 'https://www.nlsa.ac.za/isbn/', // Replace with real link when ready
       icon: 'albums',
     },
     {
       title: 'Venue Hire',
-      description: 'Want to book a venue? Click here to request a quotation',
-      link: 'https://www.nlsa.ac.za/venue-hire/', // Replace with real link when ready
+      description: 'Book our venue for your event.',
+      link: 'https://www.nlsa.ac.za/venue-hire/', 
       icon: 'easel',
     },
   ];
