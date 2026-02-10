3abf18164b05e4f8d5c330ee9d5c4568bd0d79d4
nicholusmahlangu <nicholusmahlangu@nlsa.ac.za>
2026-02-10 16:40:10 +0200

    10-2-2026 -> I added the code to retrive all the images on the latest news page and also ensure that the home page uploads as expected, since I started configuring the system from scratch. And i am able to run both back and frontend on Android studio. I also added capacitor to the project so that it convert the web App into a Mobile Application.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nlsa.app',
+  appName: 'NLSA-APP',
+  webDir: 'www'
+};
+
+export default config;
